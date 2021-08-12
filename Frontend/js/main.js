fe192878a29d268451c935877cf3e3c28d151f96
Rachel Carvalho <rachelacarvalho@gmail.com>
2021-08-12 02:08:30 -0300

    Layout do frontend criado

---
+++ Frontend/js/main.js
@@ -0,0 +1,37 @@
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     let nav = document.getElementById("nav");
+        
+//     if (window.onscroll > 2) {
+//         console.log("Rolou");
+//         nav.classList.add("active");
+//     } else {
+//         nav.classList.remove("active");
+//     }
+// });
+
+// window.addEventListener("scroll", () => {
+//     if (scroll (2, 2)) {
+//         console.log("Rolou");
+//         nav.classList.add("active");
+//     } else {
+//         nav.classList.remove("active");
+//     }
+//     }, { capture: false, passive: true})
+
+// let scrolling = false;
+// let nav = document.getElementById("nav");
+
+// window.scroll = () => {
+//     scrolling = true;
+// };
+
+// setInterval(() => {
+//     if (scrolling) {
+        
+//         console.log("Rolou");
+//         nav.classList.add("active");
+//     } else {
+//         scrolling = false;
+//         nav.classList.remove("active");
+//     }
+// },300);
